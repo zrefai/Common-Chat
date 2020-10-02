0f816f351730d29a69bc2157467931e38ef84439
Zaki <zkrefai@gmail.com>
2020-10-01 20:08:46 -0500

    Hide firebase config from files

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,17 +1,9 @@
 import "firebase/auth";
 import "firebase/firestore";
 import * as firebase from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBbuh_YHgKD2tWrjN-IN-QtzrmLqm1XHBk",
-  authDomain: "common-chat-31864.firebaseapp.com",
-  databaseURL: "https://common-chat-31864.firebaseio.com",
-  projectId: "common-chat-31864",
-  storageBucket: "common-chat-31864.appspot.com",
-  messagingSenderId: "1044717788977",
-  appId: "1:1044717788977:web:d27d6b03a13c9b583a5a10",
-  measurementId: "G-9VVTLBZQP0",
-});
+firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
